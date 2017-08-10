c677e3db4077c93c6f5315e980d559358dee4db2
LinJoseph <lyz2356002@gmail.com>
2017-08-11 00:13:22 +0800

    edit file

--- NPL/npl.py
+++ NPL/npl.py
@@ -8,10 +8,11 @@ def parser_html(html):
     items = re.findall(ur'<p>.*</p>', html.content)
     content = ""
     for i in items:
-        i=re.sub(ur"[%s%s]+" %(punctuation,string.punctuation), "", i.decode('utf-8'))
-        # i=re.sub(ur"[%s]+" %string.punctuation, "", i)
+        i = re.sub(ur"[%s%s]+" % (punctuation, string.punctuation), "", i.decode('utf-8'))
+        i = re.sub(ur"[a-z]", "", i)
         content += i[3:-4]
     return content
+
 
 if __name__ == '__main__':
     text = requests.get('http://china.huanqiu.com/article/2017-08/11108469.html?from=bdwz')
