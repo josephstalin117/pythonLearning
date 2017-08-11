6a9e75b91b93c1a693b6f1d0776e675ec76d1a45
yongze <yongze@yunshan.net.cn>
2017-08-11 14:00:19 +0800

    edit file

--- NPL/npl.py
+++ NPL/npl.py
@@ -2,6 +2,8 @@ import requests
 import re
 from zhon.hanzi import punctuation
 import string
+import jieba
+from collections import Counter
 
 
 def parser_html(html):
@@ -14,7 +16,34 @@ def parser_html(html):
     return content
 
 
+def cut(text):
+    list = jieba._lcut(text)
+    return [x.encode('utf-8') for x in list]
+
+
+def stats(list):
+    dict = {}
+    for i in list:
+        if (list.index(i)) + 1 != len(list):
+            dict[i] = list[list.index(i) + 1]
+        else:
+            dict[i] = ""
+    return dict
+
+
 if __name__ == '__main__':
     text = requests.get('http://china.huanqiu.com/article/2017-08/11108469.html?from=bdwz')
     content = parser_html(text)
     print content
+    list = cut(content)
+    # for i in list:
+    #     print i
+    # dict = Counter(list)
+    # for i in dict:
+    #     print i
+    #     print dict[i]
+    worddict = stats(list)
+    print "***************************"
+
+    for k, v in worddict.iteritems():
+        print k+" "+v
